309f3938d7eae7542e1552e4ff85d1dc2607a8aa
digitilley <jtilley451@gmail.com>
2022-01-16 14:12:11 +0000

    Add input for player name and good luck message

--- run.py
+++ run.py
@@ -1,19 +1,20 @@
 #Welcome message
 print("Welcome to Hangman!")
 
-
-
+#Rules
 rules = """
 The aim of the game is to correctly guess the random word...
----
-You get a total of 6 lives.
----
-Guess a letter to begin, if it's correct, the letter will be displayed.
----
-If you guess incorrectly, you will lose 1 life.
----
-Good luck {name}!
+
+Guess a letter to begin. If it's correct, the letter will be displayed.
+
+If you guess incorrectly, you will lose 1 life. You get a total of 6 lives.
 """
 print(rules)
+
+#Player name
+name = str(input("Enter your name: "))
+
+#Good luck message
+print("\nGood luck, "+ name)
 
 

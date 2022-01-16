220c66f50ec3c7f1f3203d16edb9f465159a287d
digitilley <jtilley451@gmail.com>
2022-01-16 14:55:06 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('random_word')
+
+words = SHEET.worksheet('words')
+
+data = words.get_all_values()
+print(data)
+
 #Welcome message
 print("Welcome to Hangman!")
 

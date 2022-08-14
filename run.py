96e5214a58e6754a5bdfbb0709e54e84ba337e3b
digitilley <jtilley451@gmail.com>
2022-08-14 12:18:28 +0000

    Add hangman graphic to show when user incorrectly guesses

--- run.py
+++ run.py
@@ -39,18 +39,47 @@ def hangman():
             turns = turns - 1
 
             if turns == 6:
-                print("6 lives left!")
+                print("6 turns left!")
+                print("---------------")
             if turns == 5:
-                print("5 lives left!")
+                print("5 turns left!")
+                print("---------------")
+                print("       O       ")
             if turns == 4:
-                print("4 lives left!")
+                print("4 turns left!")
+                print("---------------")
+                print("       O       ")
+                print("       |       ")
             if turns == 3:
-                print("3 lives left!")
+                print("3 turns left!")
+                print("---------------")
+                print("       O       ")
+                print("       |       ")
+                print("      /        ")
             if turns == 2:
-                print("2 lives left!")
+                print("2 turns left!")
+                print("---------------")
+                print("       O       ")
+                print("       |       ")
+                print("      / \      ")
             if turns == 1:
-                print("1 turns left!")
+                print("1 turn left!")
+                print("---------------")
+                print("       O /     ")
+                print("       |       ")
+                print("      / \      ")
             if turns == 0:
+                print("I'm sorry, you lose. Better luck next time!")
+                print("---------------")
+                print("     \ O /     ")
+                print("       |       ")
+                print("      / \      ")
+            if turns == 0:
+                print("Oh no!")
+                print("---------------")
+                print("       O_|     ")
+                print("     / | \     ")
+                print("      / \      ")
                 print("I'm sorry, you lose. Better luck next time!")
                 break
 
@@ -72,5 +101,5 @@ print(rules)
 # Enter name
 name = str(input("Enter your name: "))
 print("\nWelcome, " + name)
-print("\nTry to guess the word in less than 6 attempts")
+print("\nTry to guess the secret word in less than 6 attempts\n")
 hangman()
